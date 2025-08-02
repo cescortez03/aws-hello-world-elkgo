336bf7536a034853fc1e13422fabda9d26f6a43c
Cesar Cortez <cesar.cortez.-nd@disney.com>
2025-08-01 21:14:28 -0600

    Push aws ecr logic.

--- deploy/terraform/backend.tf
+++ deploy/terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-us-west-2"
+    bucket         = "terraform-state-925526501192-us-west-2"
     key            = "aws-hello-world.tfstate"
     region         = "us-west-2"
     encrypt        = true
