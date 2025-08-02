f2ba4f94c069514c7499baa706149cc59cf2a14f
Cesar Cortez <cesar.cortez.-nd@disney.com>
2025-08-01 20:40:55 -0600

    test tag

--- deploy/terraform/backend.tf
+++ deploy/terraform/backend.tf
@@ -1,3 +1,8 @@
 terraform {
-  backend "s3" {}
+  backend "s3" {
+    bucket         = "terraform-state-us-west-2"
+    key            = "aws-hello-world.tfstate"
+    region         = "us-west-2"
+    encrypt        = true
+  }
 }
--- deploy/terraform/versions.tf
+++ deploy/terraform/versions.tf
@@ -1,7 +1,17 @@
-provider "aws" {
-  default_tags {
-    tags = {
-      environment           = "development"
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.7.0"
     }
   }
 }
+
+provider "aws" {
+  default_tags {
+    tags = {
+      environment = "sandbox"
+    }
+  }
+}
+
