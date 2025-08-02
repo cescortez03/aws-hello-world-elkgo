3929f2bf7e980383bcdb29e3f72b0884e372877a
Cesar Cortez <cesar.cortez.-nd@disney.com>
2025-08-01 22:13:07 -0600

    Temp fix.

--- deploy/terraform/data.tf
+++ deploy/terraform/data.tf
@@ -4,7 +4,7 @@ data "aws_caller_identity" "current" {}
 data "aws_iam_policy_document" "lambda_role_policy" {
   statement {
     actions = [
-      "s3:PutObject",
+      "*"
     ]
     effect = "Allow"
     resources = [
