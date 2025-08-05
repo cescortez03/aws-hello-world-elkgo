9a833095d98e19029ae461f2a735fdad4b93e47b
Magdaleno <magdaleno.garcia@fraternitas.io>
2025-08-05 16:34:26 -0600

    test case 2

--- src/main.py
+++ src/main.py
@@ -2,7 +2,7 @@ import json
 import os
 def lambda_handler(event,context):
     image_tag = os.environ.get("IMAGE_TAG", "v1.x.x")
-    message = "Hola Mundo! Test case 1 {0}".format(image_tag)
+    message = "Hola Mundo! Test case 2 {0}".format(image_tag)
     return {
         "statusCode": 200,
         "body": json.dumps(message),
